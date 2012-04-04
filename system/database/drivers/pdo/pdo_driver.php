5df883227d54d8c56900fbdebc65cc4f52b4ac63
Taufan Aditya <toopay@taufanaditya.com>
2012-04-04 20:35:54 +0700

    Debug

--- system/database/drivers/pdo/pdo_driver.php
+++ system/database/drivers/pdo/pdo_driver.php
@@ -227,6 +227,7 @@ class CI_DB_pdo_driver extends CI_DB {
 		}
 
 		// Connecting...
+		var_dump('almost there..');
 		try
 		{
 			$db = new PDO($this->dsn, $this->username, $this->password, $this->options);
