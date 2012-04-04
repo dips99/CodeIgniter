5981c598309648e0f1b5d9ab8fffd29d29df1e42
Taufan Aditya <toopay@taufanaditya.com>
2012-04-04 20:24:08 +0700

    Debug

--- tests/codeigniter/database/DB_test.php
+++ tests/codeigniter/database/DB_test.php
@@ -26,9 +26,12 @@ class DB_test extends CI_TestCase {
 
 	public function test_db_valid()
 	{
+		var_dump('here');
 		$config = Mock_Database_DB::config(DB_DRIVER);
 		$connection = new Mock_Database_DB($config);
+		var_dump('there');
 		$db = Mock_Database_DB::DB($connection->set_dsn(DB_DRIVER), TRUE);
+		var_dump('there too');
 
 		$this->assertTrue($db instanceof CI_DB);
 		$this->assertTrue($db instanceof CI_DB_Driver);
