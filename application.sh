2e50ded683a4d5a392b774952c35b381bba0a8bc
Taufan Aditya <toopay@taufanaditya.com>
2012-05-07 03:30:56 +0700

    Add color to application.sh

--- application.sh
+++ application.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
+green='\e[0;32m'
+red='\e[1;31m'
+endColor='\e[0m'
+
 if test -f selenium.sh
 then
-	echo "Set Environment..."
+	echo "${green}Set Environment...${endColor}"
 	export DISPLAY=:99.0
 	sh -e /etc/init.d/xvfb start
 	pyrus install http://pear.phpunit.de/get/PHPUnit_Selenium-1.2.6.tgz
 	sudo apt-get -qq update
-	echo "Environment OK"
-	echo "Set Web Server..."
+	echo "${green}Environment OK${endColor}"
+	echo "${green}Set Web Server...${endColor}"
 	sudo apt-get -y -qq install apache2
 	sudo apt-get -y -qq install libapache2-mod-php5
 	sudo /etc/init.d/apache2 restart
-	echo "Web Server OK"
-	echo "Set Up CodeIgniter on Root Directory..."
+	echo "${green}Web Server OK${endColor}"
+	echo "${green}Set Up CodeIgniter on Root Directory...${endColor}"
 	sudo mkdir /var/www/codeigniter
 	sudo chmod 777 /var/www/codeigniter -R
 	sudo cp index.php /var/www/codeigniter/index.php
 	sudo cp application/ /var/www/codeigniter/ -r
 	sudo cp system/ /var/www/codeigniter/ -r
-	echo "CodeIgniter OK"
-	echo "Set Up Selenium..."
+	echo "${green}CodeIgniter OK${endColor}"
+	echo "${green}Set Up Selenium...${endColor}"
 	wget http://selenium.googlecode.com/files/selenium-server-standalone-2.21.0.jar
 	sudo mkdir /usr/lib/selenium
 	sudo cp selenium-server-standalone-2.21.0.jar /usr/lib/selenium/selenium-server-standalone-2.21.0.jar
@@ -31,8 +35,8 @@ then
 	/etc/init.d/selenium restart
 	ps aux | grep 2.21
 	sleep 5	
-	echo "Selenium OK"
-	echo "Application Test READY"
+	echo "${green}Selenium OK${endColor}"
+	echo "${green}Application Test READY${endColor}"
 else
-	echo "Application Test NOT READY"
+	echo "${red}Application Test NOT READY${endColor}"
 fi
